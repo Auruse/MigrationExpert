0a0239919e266297bbbfe16a845b2e6491816ee0
puzin <pam8@yandex.ru>
2019-05-16 19:14:04 +0300

    1

---
+++ MigrationExpert/Models/FormN.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MigrationExpert.Models
+{
+    public class FormN
+    {
+        string Name { get; set; }
+
+    }
+}
