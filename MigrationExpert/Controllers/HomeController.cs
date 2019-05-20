082bc7e9d06b8d77daf8b81083e1d8aa00b5acec
Anatoly Osipov <pam8@yandex.ru>
2019-05-20 23:19:04 +0300

    2

--- MigrationExpert/Controllers/HomeController.cs
+++ MigrationExpert/Controllers/HomeController.cs
@@ -23,12 +23,20 @@ namespace MigrationExpert.Controllers
 
         [HttpPost]
         public ActionResult Index(FormN FormData)
-        {
-             
-            ViewBag.NameD = FormData.Surname.ToUpper() ?? " ";
-            ViewBag.NameP = FormData.Name.ToUpper();
-            ViewBag.CitizenShip = FormData.Citizenship.ToUpper();
+        {             
+            ViewBag.NameD = FormData.Surname?.ToUpper() ?? " ";
+            ViewBag.NameP = FormData.Name?.ToUpper() ?? " ";
+            ViewBag.CitizenShip = FormData.Citizenship?.ToUpper() ?? " ";
             ViewBag.DateofBirth = FormData.DateOfBirth;
+            ViewBag.PGender = FormData.YourGender.ToString() ?? "male";
+            ViewBag.CountryOfBirth = FormData.CountryOfBirth?.ToUpper() ?? " ";
+            ViewBag.CityOfBirth = FormData.CityOfBirth?.ToUpper() ?? " ";
+            ViewBag.PersonalDocumentSeries = FormData.PersonalDocumentSeries?.ToUpper() ?? " ";
+            ViewBag.PersonalDocumentType = FormData.PersonalDocumentType?.ToUpper() ?? " ";
+            ViewBag.PersonalDocumentNumber = FormData.PersonalDocumentNumber?.ToUpper() ?? " ";
+            ViewBag.PersonalDocumentDateOfIssue = FormData.PersonalDocumentDateOfIssue;
+            ViewBag.PersonalDocumentDateOfExpiration = FormData.PersonalDocumentDateOfExpiration;
+            ViewBag.YourVisa = FormData.YourVisa.ToString() ?? "visa";
             return View("Index1");
         }
 
